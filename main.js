bd924ddebe3f59d29963c5873e632c05c425c459
lateepharh <pelumilateefat299@gmail.com>
2022-09-19 13:55:49 -0700

    committed

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ images.forEach((item,i)=>{
       imageslider.style.backgroundImage = `url('${item.src}')`;
       activeImage= i;
    })
-})
+});
 const sizeBtn = document.querySelectorAll(".size-radio-btn");
 let checkedbtn = 0;
 sizeBtn.forEach((items,i)=>{
@@ -18,4 +18,25 @@ sizeBtn.forEach((items,i)=>{
       items.classList.add("check");
       checkedbtn = i;
    })
-})
+});
+// javacript logic for automated slideshow
+let slideIndex = 0;
+showSlides();
+function showSlides(){
+   let y;
+   let slides = document.getElementsByClassName("mySlides");
+   let dots = document.getElementsByClassName("dot");
+   for(y = 0; y < slides.length; y++){
+      slides[y].style.display = "none"
+   };
+   slideIndex++;
+   if (slideIndex > slides.length ) {
+      slideIndex = 1;
+   };
+   for(i = 0; i < dots.length; i++ ){
+      dots[i].className = dots[i].className.replace("active");
+   }
+   slides[slideIndex - 1].style.display ="block";
+   dots[slideIndex - 1 ].className += "active";
+   setTimeout(showSlides, 2000)
+}
