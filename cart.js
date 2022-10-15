3c2eafb2a7c193279e7f1b64b4ea035712cf1f88
lateepharh <pelumilateefat299@gmail.com>
2022-10-15 10:46:57 -0700

    committed

--- cart.js
+++ cart.js
@@ -21,19 +21,19 @@ var product = [
     {
        name:'Casual Men Jacket',
        price:9000,
-       tag:"menjac",
+       tag:"men jac",
        inCart:0
     },
     {
        name:'Stripe Shirt',
        price:6000,
-       tag:"men top 1",
+       tag:"men top1",
        inCart:0
     },
     {
        name:'Round Neck Polo',
        price:10000,
-       tag:"product image3",
+       tag:"product image 3",
        inCart:0
     },
     {
@@ -82,23 +82,22 @@ for (let i = 0; i < carts.length; i++){
 };
 // it shows number of iten in the cart
 function onloadCartNumber(){
-    let productNumbers = localStorage.getItem('cartNumbers');
-    if (productNumbers) {
-       document.querySelector('.cartss span').textContent = productNumbers;
-       
-    }
+   let productNumbers = localStorage.getItem('cartNumbers');
+   if (productNumbers) {
+      document.querySelector('.cartss sup').textContent = productNumbers;    
+   }
  }
  // when the add to cart is clicked the function helps to store it
- function cartNumbers(product){
-    console.log("the product clicked is", product);
-    let productNumbers = localStorage.getItem('cartNumbers');
-    productNumbers = parseInt(productNumbers);
-    if (productNumbers) {
-       localStorage.setItem('cartNumbers',productNumbers + 1);
-       document.querySelector('.cartss span').textContent = productNumbers + 1;
-    }else{
-       localStorage.setItem('cartNumbers', 1);
-       document.querySelector('.cartss span').textContent = 0;
+function cartNumbers(product){
+   // console.log("the product clicked is", product);
+   let productNumbers = localStorage.getItem('cartNumbers');
+   productNumbers = parseInt(productNumbers);
+   if (productNumbers) {
+      localStorage.setItem('cartNumbers',productNumbers + 1);
+      document.querySelector('.cartss sup').textContent = productNumbers + 1;
+   }else{
+      localStorage.setItem('cartNumbers', 1);
+       document.querySelector('.cartss sup').textContent = 0;
     }
     // localStorage.setItem('cartNumbers',1)
     setItems(product)
@@ -135,32 +134,80 @@ function totalCost(products){
     }
     // console.log("product price is", products.price);
 }
+// function for the cart pge
 function displyCart() {
-    let cartItems = localStorage.getItem("productInCart");
-    cartItems = JSON.parse(cartItems);
-    // console.log(cartItems);
-    let productContainer = document.querySelector('.cart-items');
+   let cartItems = localStorage.getItem("productInCart");
+   cartItems = JSON.parse(cartItems);
+   // console.log(cartItems);
+   // var cartRow = document.createElement('div')
+   // cartRow.classList.add('cart-row')
+   let productContainer = document.querySelector('.products');
    let cartCost = localStorage.getItem('totalCost');
    // the if checks if the product-container is on the page and if our cartitems got something
    if (cartItems && productContainer) {
       // console.log("running");
       productContainer.innerHTML = "";
-      Object.values(cartItems).map(item =>{
-           productContainer.innerHTML += `
-           <div class="cart-item cart-column">
-               <i class="fa fa-times-circle"></i>
-               <img src="../picture/${item.tag}.jpg" class="cart-item-image">
-              <span class="cart-item-title">${item.name}</span>
-           </div>
-           <div class="cart-pricess cart-column">${item.price}</div>
-           <div class="cart-quantity cart-column">
-               <input class="cart-quantity-input" type="number" value="1">
-               <span>${item.inCart}</span>
-            </div>
-           `
-        })
-    }
+      Object.values(cartItems).forEach(item =>{
+   // let conts = document.querySelector('.product-container');
+      productContainer.innerHTML += `
+         <div class="product">
+            <i class="fa fa-times-circle times-icon" id="btn-danger" ></i>
+            <img src="../pic/${item.tag}.png" alt="product" >
+            <span>${item.name}</span>
+         </div>
+         <div class="price ">${item.price}</div>
+         <div class="quantity">
+            <i class="fa fa-arrow-circle-left decrease"></i>
+            <span>${item.inCart}</span>
+            <i class="fa fa-arrow-circle-right increase"></i>
+         </div>
+         <div class="total">#${item.inCart * item.price}.00</div>
+
+         `;
+      });
+      productContainer.innerHTML += `
+      <div class="basketTotalContainer">
+         <h4 class="basketTotalTitle">
+            Total
+         </h4>
+         <h4 class="basketTotalTitle">#${cartCost}.00</h4>
+      </div>
+      <button class="purchase-btn" id="purchase-btn">
+         Checkout
+      </buuton>
+      `;
+   }
 }
+function removeButton (){
+   let button = document.getElementById("btn-dander");
+   button.addEventListener("click", ()=>{
+      alert("clicked")
+  });
+      
+}
+function Purchased(){
+   let btn = document.getElementById("purchase-btn");
+   btn.addEventListener("click",()=>{
+      alert("Thank you for your purchase");
+      let productContainer = document.querySelector('.products');
+      while (productContainer.hasChildNodes()) {
+         productContainer.removeChild(productContainer.firstChild);
+      }
+      let productNumbers = localStorage.getItem('cartNumbers');
+       let cartItems =localStorage.getItem('productInCart');
+      if (productNumbers) {
+        productNumbers = localStorage.clear('cartNumbers' );
+        document.querySelector('.cartss sup').textContent = 0;
+      }
+      if (cartItems) {
+         cartItems = localStorage.clear("productInCart")
+      }
+      
+   });
+  
+};
 onloadCartNumber();
-displyCart(); 
+displyCart();
+// removeButton();
+Purchased(); 
  
